21ebfa28cd4a12c3508fa701e4cabe2b0561c703
Jay Shollenberger <jshollen@redhat.com>
2021-06-15 17:17:03 -0600

    openshift, call count, img build scripts

---
+++ pushimage.sh
@@ -0,0 +1,3 @@
+#!/bin/sh 
+docker tag servicemesh-leaf2:latest docker.io/sholly/servicemesh-leaf2:1.0
+docker push docker.io/sholly/servicemesh-leaf2:1.0
--- src/main/java/com/redhat/demo/servicemeshleaf2/controller/LeafController.java
+++ src/main/java/com/redhat/demo/servicemeshleaf2/controller/LeafController.java
@@ -5,20 +5,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class LeafController {
+
+    private static AtomicLong callCount = new AtomicLong(0);
 
     @Autowired
     private ClientService clientService;
 
     @GetMapping("/leaf2")
     public String leaf2() {
-        return "leaf2";
+        return new StringBuilder("leaf2")
+                .append(", call Count: " + callCount.incrementAndGet())
+                .toString();
     }
 
     @GetMapping("callleaf1")
     public String callLeaf1() {
         String response = clientService.callLeaf1();
+        StringBuilder finalResponse = new StringBuilder(response).append("\n")
+                .append("call count: " + callCount.incrementAndGet());
         return response;
     }
 }
