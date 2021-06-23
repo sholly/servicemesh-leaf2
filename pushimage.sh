5843793f92a7db4a54fe34772cb1fcc430be051d
Jay Shollenberger <jshollen@redhat.com>
2021-06-23 08:48:48 -0600

    jaeger/prometheus works

--- pushimage.sh
+++ pushimage.sh
@@ -1,3 +1,3 @@
 #!/bin/sh 
-docker tag servicemesh-leaf2:latest docker.io/sholly/servicemesh-leaf2:1.0
-docker push docker.io/sholly/servicemesh-leaf2:1.0
+docker tag servicemesh-leaf2:latest docker.io/sholly/servicemesh-leaf2:jaeger
+docker push docker.io/sholly/servicemesh-leaf2:jaeger
